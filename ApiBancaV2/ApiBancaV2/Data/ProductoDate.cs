31f41f8491c3ba52790e70522df20b0df75ee4fb
AlvaroJoelGL <ag2017057608@virtual.upt.pe>
2023-05-06 22:12:45 -0500

    Creacion de Proyecto con Productos

---
+++ ApiBancaV2/ApiBancaV2/Data/ProductoDate.cs
@@ -0,0 +1,120 @@
+﻿using ApiBanca.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ApiBanca.Data
+{
+    public class ProductoDate
+    {
+        public static bool Registrar(Productos oProducto)
+        {
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_registrar", oConexion);
+                cmd.CommandType=CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", oProducto.nombre);
+                cmd.Parameters.AddWithValue("@descripcion", oProducto.descripcion);
+                cmd.Parameters.AddWithValue("@tipo", oProducto.tipo);
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static Productos Obtener(int idproducto)
+        {
+            Productos oProducto = new Productos();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_obtener", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", idproducto);
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            oProducto = new Productos()
+                            {
+                                id = Convert.ToInt32(dr["id"]),
+                                nombre = dr["nombre"].ToString(),
+                                descripcion = dr["descripcion"].ToString(),
+                                tipo = dr["tipo"].ToString(),
+                            };
+                        }
+
+                    }
+
+
+
+                    return oProducto;
+                }
+                catch (Exception ex)
+                {
+                    return oProducto;
+                }
+            }
+        }
+
+        public static List<Productos> Listar()
+        {
+            List<Productos> oListaProducto = new List<Productos>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_listar", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            oListaProducto.Add(new Productos()
+                            {
+                                id = Convert.ToInt32(dr["id"]),
+                                nombre = dr["nombre"].ToString(),
+                                descripcion = dr["descripcion"].ToString(),
+                                tipo = dr["tipo"].ToString(),
+                            });
+                        }
+
+                    }
+
+
+
+                    return oListaProducto;
+                }
+                catch (Exception ex)
+                {
+                    return oListaProducto;
+                }
+            }
+        }
+
+        
+
+    }
+}
